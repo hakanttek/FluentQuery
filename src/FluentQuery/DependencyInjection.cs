73e9fc981b655b5f877c01bc9f95b6489eaff692
hakanttek <hakanttek@gmail.com>
2025-05-09 01:19:55 +0200

    Add Executor registration tracking and DI extension
    Updated `ExecutorOptions` to include `ExecutorRegistered` property for tracking executor registration status. Modified `Use<TExecutor>` method to set this property when an executor is used.
    
    Introduced a new static class `DependencyInjection` with an `AddFluentQuery` extension method for `IServiceCollection`. This method validates executor registration before adding options as a singleton service, enhancing the dependency injection setup.

---
+++ src/FluentQuery/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace FluentQuery;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddFluentQuery(this IServiceCollection services, Action<ExecutorOptions> setupAction)
+    {
+        var options = new ExecutorOptions(services);
+        setupAction(options);
+
+        if(!options.ExecutorRegistered)
+            throw new InvalidOperationException("Executor not registered.");
+
+        return services.AddSingleton(options);
+    }
+}
--- src/FluentQuery/ExecutorOptions.cs
+++ src/FluentQuery/ExecutorOptions.cs
@@ -14,8 +14,11 @@ public class ExecutorOptions
 
     public string ConnectionString { get; set; } = string.Empty;
 
+    internal bool ExecutorRegistered { get; private set; } = false;
+
     public void Use<TExecutor>(ServiceLifetime serviceLifetime) where TExecutor : class, IExecutor
     {
+        ExecutorRegistered = true;
         switch (serviceLifetime)
         {
             case ServiceLifetime.Transient:
