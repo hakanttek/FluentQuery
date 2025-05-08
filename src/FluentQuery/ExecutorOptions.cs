46ce52325cdcd61c9c5055f7f476b4019408b396
hakanttek <hakanttek@gmail.com>
2025-05-09 01:14:50 +0200

    Add FluentQuery.InMemory project to solution
    Introduced a new project "FluentQuery.InMemory" with its project file path and unique identifier. Updated solution and project configuration settings accordingly, and nested it under the existing "src" project.

---
+++ src/FluentQuery/ExecutorOptions.cs
@@ -0,0 +1,32 @@
+﻿using FluentQuery.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FluentQuery;
+
+public class ExecutorOptions
+{
+    public ExecutorOptions(IServiceCollection services)
+    {
+        Services = services;
+    }
+
+    protected readonly IServiceCollection Services;
+
+    public string ConnectionString { get; set; } = string.Empty;
+
+    public void Use<TExecutor>(ServiceLifetime serviceLifetime) where TExecutor : class, IExecutor
+    {
+        switch (serviceLifetime)
+        {
+            case ServiceLifetime.Transient:
+                Services.AddTransient<IExecutor, TExecutor>();
+                break;
+            case ServiceLifetime.Scoped:
+                Services.AddScoped<IExecutor, TExecutor>();
+                break;
+            case ServiceLifetime.Singleton:
+                Services.AddSingleton<IExecutor, TExecutor>();
+                break;
+        }
+    }
+}
