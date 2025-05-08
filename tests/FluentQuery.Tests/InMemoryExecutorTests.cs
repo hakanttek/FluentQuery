e366564ba62b5f8d5de187c71c782063252bed02
hakanttek <hakanttek@gmail.com>
2025-05-09 01:34:25 +0200

    Add dependency injection and new in-memory tests
    Updated `FluentQuery.Tests.csproj` to include package references for `Microsoft.Extensions.DependencyInjection` and `Microsoft.Extensions.DependencyInjection.Abstractions`. Added project references to `FluentQuery.InMemory.csproj` and `FluentQuery.csproj`.
    
    Introduced `InMemoryExecutorTests` class with a setup method to configure FluentQuery for in-memory execution and retrieve an `IExecutor` instance for testing.

---
+++ tests/FluentQuery.Tests/InMemoryExecutorTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentQuery.InMemory;
+using FluentQuery.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FluentQuery.Tests;
+
+public class InMemoryExecutorTests
+{
+    private IExecutor _executor;
+
+    [SetUp]
+    public void Setup()
+    {
+        var services = new ServiceCollection();
+        services.AddFluentQuery(options =>
+        {
+            options.UseInMemory();
+        });
+
+        var serviceProvider = services.BuildServiceProvider();
+
+        _executor = serviceProvider.GetRequiredService<IExecutor>();
+    }
+}
