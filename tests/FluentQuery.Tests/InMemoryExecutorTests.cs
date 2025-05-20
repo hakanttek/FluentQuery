8e5afc64906b1a087d069149ef2a88a97a4a74cf
hakanttek <hakanttek@gmail.com>
2025-05-20 08:52:46 +0200

    Refactor user table test for improved performance
    Updated `ExecuteAsync_ShouldCreateTable_InsertEntity_AndReturnExpectedResult` method to streamline SQL commands and enhance performance by using `FirstOrDefaultAsync()`. Adjusted test case attributes accordingly.

--- tests/FluentQuery.Tests/InMemoryExecutorTests.cs
+++ tests/FluentQuery.Tests/InMemoryExecutorTests.cs
@@ -21,27 +21,23 @@ public class InMemoryExecutorTests
         _executor = serviceProvider.GetRequiredService<IExecutor>();
     }
 
-    [TestCase(@"
+    [Test]
+    public async Task ExecuteAsync_ShouldCreateTable_InsertEntity_AndReturnExpectedResult()
+    {
+        // Arrange
+        var createTableSql = @"
         CREATE TABLE Users (
             Id INTEGER PRIMARY KEY AUTOINCREMENT,
             FullName TEXT
         );
         INSERT INTO Users (FullName) VALUES ('John Doe');
-        ",
-        "SELECT * FROM Users",
-        "FullName",
-        "John Doe",
-        TestName = "UserTable")]
-    public async Task ExecuteAsync_ShouldCreateTable_InsertEntity_AndReturnExpectedResult(
-        string createTableSql,
-        string selectAllSql,
-        string key,
-        string value)
-    {
+        ";
+
+        var selectAllSql = @"SELECT * FROM Users";
+
         // Act
         await _executor.ExecuteAsync(createTableSql);
-        var users = await _executor.Execute<User>(selectAllSql).ToListAsync();
-        var user = users.FirstOrDefault();
+        var user = await _executor.Execute<User>(selectAllSql).FirstOrDefaultAsync();
 
         // Assert
         Assert.Multiple(() =>
