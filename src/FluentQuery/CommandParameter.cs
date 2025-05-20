158973a83810ee9f437ad298155d3ed646b5a25c
hakanttek <hakanttek@gmail.com>
2025-05-21 00:56:53 +0200

    Add CommandParameter<T> record to FluentQuery namespace
    Introduces a new record type `CommandParameter<T>` with properties
    `Name`, `Value`, and `Type`. The constructor initializes these
    properties, with `Type` defaulting to the result of
    `value.GetDbType()` if not provided.

---
+++ src/FluentQuery/CommandParameter.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+
+namespace FluentQuery;
+
+public record CommandParameter<T>
+{
+    public readonly string Name;
+
+    public readonly T? Value;
+
+    public readonly DbType Type;
+
+    public CommandParameter(string name, T? value, DbType? type = null)
+    {
+        Name = name;
+        Value = value;
+        Type = type ?? value.GetDbType();
+    }
+}
