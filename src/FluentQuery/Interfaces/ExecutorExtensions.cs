8255e556c47cc5a4c30999b51fdee15a8ec4bc37
hakanttek <hakanttek@gmail.com>
2025-05-21 02:15:12 +0200

    Enhance IExecutor with default CancellationToken support
    Updated the `IExecutor<TContext>` interface to include a new `Execute<T>` method that allows omitting the `CancellationToken` parameter by providing a default value. Introduced the `ExecutorExtensions` class with an extension method for `ExecuteAsync`, enabling calls without explicitly passing a cancellation token. This improves usability and streamlines query execution.

---
+++ src/FluentQuery/Interfaces/ExecutorExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace FluentQuery.Interfaces;
+
+public static class ExecutorExtensions
+{
+    public static Task ExecuteAsync<TContext>(this IExecutor<TContext> executor, string query, params CommandParameter[] parameters)
+        => executor.ExecuteAsync(query, default, parameters);
+}
--- src/FluentQuery/Interfaces/IExecutor.cs
+++ src/FluentQuery/Interfaces/IExecutor.cs
@@ -5,6 +5,8 @@ public interface IExecutor<TContext>
     public Task ExecuteAsync(string query, CancellationToken cancellation = default, params CommandParameter[] parameters);
 
     public IAsyncEnumerable<T> Execute<T>(string query, CancellationToken cancellation = default, params CommandParameter[] parameters);
+
+    public IAsyncEnumerable<T> Execute<T>(string query, params CommandParameter[] parameters) => Execute<T>(query, default, parameters);
 }
 
 public interface IExecutor : IExecutor<ExecutorContext>
