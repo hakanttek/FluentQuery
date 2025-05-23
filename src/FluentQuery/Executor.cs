5c658bf5a4be4c698e7959da097529c7b036d317
hakanttek <hakanttek@gmail.com>
2025-05-23 17:34:30 +0200

    Fix target frameworks and add dependency injection support
    Updated `FluentQuery.csproj` to remove duplicate `net6.0` entry and added `Microsoft.Extensions.DependencyInjection` package references for .NET 6, 7, 8, and 9.
    
    Introduced `Executor<TContext>` class in `Executor.cs` to manage and retrieve `IExecutor<TContext>` instances using dependency injection.

---
+++ src/FluentQuery/Executor.cs
@@ -0,0 +1,15 @@
+﻿using FluentQuery.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FluentQuery;
+
+public static class Executor<TContext> where TContext : ExecutorContext
+{
+    private static readonly IServiceCollection Services = new ServiceCollection().AddFluentQuery();
+
+    private static readonly Lazy<IServiceProvider> LazyProvider = new(Services.BuildServiceProvider);
+
+    private static IServiceProvider ServiceProvider => LazyProvider.Value;
+
+    public static IExecutor<TContext> Static => ServiceProvider.GetRequiredService<IExecutor<TContext>>();
+}
