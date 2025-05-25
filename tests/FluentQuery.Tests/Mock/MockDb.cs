72d91fbbf5afd9aed52a6d753f080c79c3e68e8f
hakanttek <hakanttek@gmail.com>
2025-05-25 03:25:23 +0200

    Add StaticExecutorTests for SQL command execution
    Introduce `StaticExecutorTests` with tests for executing SQL commands using a static executor. The tests validate the creation of an in-memory database, the creation of a `Users` table, and the retrieval of user data. Also, add `MockDb` class to configure the context for in-memory usage.

---
+++ tests/FluentQuery.Tests/Mock/MockDb.cs
@@ -0,0 +1,11 @@
+﻿using FluentQuery.InMemory;
+
+namespace FluentQuery.Tests.Mock;
+
+internal class MockDb : StaticExecutorContext
+{
+    public override void Configure(StaticExecutorContext context)
+    {
+        context.UseInMemory();
+    }
+}
---
+++ tests/FluentQuery.Tests/StaticExecutorTests.cs
@@ -0,0 +1,77 @@
+﻿using FluentQuery.InMemory;
+using FluentQuery.Interfaces;
+using FluentQuery.Tests.Mock;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FluentQuery.Tests;
+
+public class StaticExecutorTests
+{
+    private async Task<bool> TryCrateTable(string sql)
+    {
+        try
+        {
+            await Executor<MockDb>.Static.ExecuteAsync(sql);
+            return true;
+        }
+        catch (SqliteException)
+        {
+            return false;
+        }
+    }
+
+    [Test]
+    public async Task ExecuteAsync_ShouldReturnExpectedUser()
+    {
+        // Arrange
+        var createTableSql = @"
+        CREATE TABLE Users (
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            FullName TEXT
+        );
+        INSERT INTO Users (FullName) VALUES ('John Doe');
+        ";
+
+        var selectAllSql = @"SELECT * FROM Users";
+
+        // Act
+        await TryCrateTable(createTableSql);
+        var user = await Executor<MockDb>.Static.Execute<User>(selectAllSql).FirstOrDefaultAsync();
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(user, Is.Not.Null);
+            Assert.That(user?.Id, Is.Not.Null);
+            Assert.That(user?.FullName, Is.EqualTo("John Doe"));
+        });
+    }
+
+    [Test]
+    public async Task ExecuteAsync_WithParameter_ShouldReturnExpectedUser()
+    {
+        // Arrange
+        var createTableSql = @"
+        CREATE TABLE Users (
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            FullName TEXT
+        );
+        INSERT INTO Users (FullName) VALUES ('John Doe');
+        ";
+
+        var selectAllSql = @"SELECT * FROM Users WHERE FullName = @fullName";
+
+        // Act
+        await TryCrateTable(createTableSql);
+        var user = await Executor<MockDb>.Static.Execute<User>(selectAllSql, "John Doe".ToParam("fullName")).FirstOrDefaultAsync();
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(user, Is.Not.Null);
+            Assert.That(user?.Id, Is.Not.Null);
+            Assert.That(user?.FullName, Is.EqualTo("John Doe"));
+        });
+    }
+}
